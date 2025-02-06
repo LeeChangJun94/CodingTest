257cbbb0a36af3082eba3319622e435b18c13502
LeeChangJun94 <121155662+LeeChangJun94@users.noreply.github.com>
2025-02-07 02:02:32 +0900

    [level 0] Title: 뒤에서 5등 위로, Time: 0.02 ms, Memory: 3.64 MB -BaekjoonHub

---
+++ 프로그래머스/0/181852. 뒤에서 5등 위로/뒤에서 5등 위로.cpp
@@ -0,0 +1,20 @@
+#include <string>
+#include <vector>
+#include <algorithm>
+
+using namespace std;
+
+vector<int> solution(vector<int> num_list)
+{
+    vector<int> answer;
+    answer.reserve(num_list.size());
+    
+    sort(num_list.begin(), num_list.end());
+    
+    for(int i = 5; i < num_list.size(); i++)
+    {
+        answer.push_back(num_list[i]);
+    }
+    
+    return answer;
+}
